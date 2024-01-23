1e7409265d7ea807b8838ba6c324ba4be59b1734
Tobias Roeske <t.roeske91@gmail.com>
2024-01-23 18:47:01 +0100

    Functions to change pages are kinda working, but need fixing

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let currentPokemon;
 let pokemonList = [];
+let limit = 25;
+let offset = 0;
 // let types = [
 //     "Electric",
 //     "Poison",
@@ -27,7 +29,8 @@ async function init() {
 
 async function loadAllPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/'
-    for (let i = 1; i <= 20; i++) {
+    pokemonList = [];
+    for (let i = offset + 1; i <= limit; i++) {
         let response = await fetch(`${url}${i}`);
         if (response.ok) {
             currentPokemon = await response.json();
@@ -38,12 +41,48 @@ async function loadAllPokemon() {
     }
 }
 
+async function changePage(start, end) {
+    await init(start, end)
+}
+// Die beiden Funktionen funktionieren noch nicht!
+async function showNextPage() {
+    let nextPageButton = document.getElementById('nextPage');
+    let previousPageButton = document.getElementById('previousPage');
+    if (limit < 125) {
+        previousPageButton.classList.remove('d-none');
+        offset += 25;
+        limit += 25;
+        await init();
+    } else {
+        offset = 125;
+        limit = 151;
+        await init();
+        nextPageButton.classList.add('d-none');
+    }
+}
+
+async function showPreviousPage() {
+    let previousPageButton = document.getElementById('previousPage');
+    if (limit > 50) {
+        previousPageButton.classList.remove('d-none');
+        offset-= 25;
+        limit -= 25;
+        await init();
+    } else {
+        offset -= 25;
+        limit -= 25;
+        await init();
+        previousPageButton.classList.add('d-none');
+    }
+}
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function renderPokemon() {
     let content = document.getElementById('content');
+    content.innerHTML = '';
     for (let i = 0; i < pokemonList.length; i++) {
         let pokemon = pokemonList[i];
         content.innerHTML += generateOverviewHTML(pokemon);
