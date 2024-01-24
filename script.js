b96220616b9729ba78d8284fb6745bffb5a4c39d
Tobias Roeske <t.roeske91@gmail.com>
2024-01-24 16:59:55 +0100

    Pokemon Card popup implemented

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let currentPokemon;
 let pokemonList = [];
+let pokemonInfos = [];
 let limit = 25;
 let offset = 0;
 let currentPage = 1;
 
 async function init() {
-    await loadAllPokemon();
+    await loadPokemon();
     renderPokemon();
 }
 
-async function loadAllPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/'
+async function loadPokemon() {
+    let url = `https://pokeapi.co/api/v2/pokemon/`
     pokemonList = [];
     for (let i = offset + 1; i <= limit; i++) {
         let response = await fetch(`${url}${i}`);
@@ -80,12 +81,20 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function closeCard() {
+    document.getElementById('popup').classList.add('d-none');
+}
+
+function doNotClose(event) {
+    event.stopPropagation();
+}
+
 function renderPokemon() {
     let content = document.getElementById('content');
     content.innerHTML = '';
     for (let i = 0; i < pokemonList.length; i++) {
         let pokemon = pokemonList[i];
-        content.innerHTML += generateOverviewHTML(pokemon);
+        content.innerHTML += generateOverviewHTML(pokemon, i);
     }
 }
 
@@ -100,9 +109,9 @@ function typeHTML(pokemon) {
     return htmlText;
 }
 
-function generateOverviewHTML(pokemon) {
+function generateOverviewHTML(pokemon, i) {
     return /*html*/`
-    <div class="poke-container-small ${pokemon['types'][0]['type']['name']}" id="${pokemon['name']}Overview">
+    <div class="poke-container-small ${pokemon['types'][0]['type']['name']}" id="${pokemon['name']}Overview" onclick="openPokemonCard(${i})">
         <span class="pokeNumber">#${pokemon['id']}</span>
         <h2>${capitalizeFirstLetter(pokemon['name'])}</h2>
         <div class="poke-details" id="pokeDetails">
@@ -113,4 +122,66 @@ function generateOverviewHTML(pokemon) {
         </div>
     </div>
 `;
+}
+
+async function openPokemonCard(index) {
+    let popup = document.getElementById('popup');
+    let pokemon = pokemonList[index];
+    popup.classList.remove('d-none');
+    popup.innerHTML = '';
+    popup.innerHTML += await generatePokemonCardHTML(pokemon, index);
+}
+
+async function getPokemonDetails(param, index) {
+    let id = index + 1
+    let url = `https://pokeapi.co/api/v2/${param}/${id}`
+    let response = await fetch(url);
+    if (response.ok) {
+        let pokemonInfo = await response.json();
+        return pokemonInfo;
+    } else {
+        alert('It seems like something went wrong, could not find the pokemon')
+        return null;
+    }
+}
+
+
+async function generatePokemonCardHTML(pokemon, index) {
+    return /*html*/`
+        <div class="popup-card" id="${pokemon['name']}Card" onclick="doNotClose(event)">
+            <div class="${pokemon['types'][0]['type']['name']} popup-card-top">
+                <div class="pokemon-overview">
+                    <div class="name-type">
+                     <h2>${capitalizeFirstLetter(pokemon['name'])}</h2>
+                        <div>
+                            ${typeHTML(pokemon)}
+                        </div>
+                    </div>
+                    <span>#${pokemon['id']}</span>
+                </div>
+                <img src="${pokemon['sprites']['other']['dream_world']['front_default']}" class="pokemon-card-img">
+                <img src="img/pokeball_white_100.png" alt="" class="pokeball-img">
+            </div>
+            <div class="pokemon-card-bottom">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Base Stats</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Evolution</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Moves</a>
+                        </li>
+                    </ul>
+                    <div id="pokemonInfo">
+                    
+                    </div>
+            </div>
+            
+        </div>
+    `
 }
